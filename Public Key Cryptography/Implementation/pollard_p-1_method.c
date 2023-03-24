586f2a3696bba0cf9509203a2784071802383855
Ji, Yong-Hyeon <60635415+Hacker-Code-J@users.noreply.github.com>
2023-03-24 13:57:32 +0900

    Update pollard_p-1_method.c

--- Public Key Cryptography/Implementation/pollard_p-1_method.c
+++ Public Key Cryptography/Implementation/pollard_p-1_method.c
@@ -1,52 +1,58 @@
 #include <stdio.h>
-#include <gmp.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 
-void pollard_p_1(mpz_t N, unsigned long k, mpz_t p, mpz_t q) {
-    mpz_t a, d, j, one;
-    mpz_inits(a, d, j, one, NULL);
+uint64_t gcd(uint64_t a, uint64_t b) {
+    while (b != 0) {
+        uint64_t t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
 
-    mpz_set_ui(a, 2);
-    mpz_set_ui(j, 2);
-    mpz_set_ui(one, 1);
+uint64_t mod_exp(uint64_t base, uint64_t exp, uint64_t mod) {
+    uint64_t result = 1;
+    base %= mod;
 
-    while (mpz_cmp_ui(j, k) < 0) {
-        mpz_powm(a, a, j, N);
-        mpz_sub_ui(a, a, 1);
-        mpz_gcd(d, a, N);
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
 
-        if (mpz_cmp(d, one) > 0 && mpz_cmp(d, N) < 0) {
-            mpz_set(p, d);
-            mpz_divexact(q, N, d);
+void pollard_p_1(uint64_t N, uint64_t k, uint64_t *p, uint64_t *q) {
+    uint64_t a = 2;
+    uint64_t j = 2;
+
+    while (j < k) {
+        a = mod_exp(a, j, N);
+        uint64_t d = gcd(a - 1, N);
+
+        if (d > 1 && d < N) {
+            *p = d;
+            *q = N / d;
             break;
         }
 
-        mpz_add_ui(a, a, 1);
-        mpz_add_ui(j, j, 1);
+        a = (a + 1) % N;
+        j++;
     }
-
-    mpz_clears(a, d, j, one, NULL);
 }
 
 int main() {
-    mpz_t N, p, q;
-    unsigned long k = 1000;
+    uint64_t N = 7420738134810; // This is just an example value for N
+    uint64_t k = 1000;
+    uint64_t p, q;
 
-    gmp_randstate_t state;
-    gmp_randinit_default(state);
-    gmp_randseed_ui(state, time(NULL));
+    pollard_p_1(N, k, &p, &q);
 
-    mpz_inits(N, p, q, NULL);
-    mpz_rrandomb(N, state, 40); // Generate a 40-bit random number
-    mpz_nextprime(N, N);        // Get the next prime larger than N
-
-    pollard_p_1(N, k, p, q);
-
-    gmp_printf("N = %Zd = %Zd * %Zd\n", N, p, q);
-
-    mpz_clears(N, p, q, NULL);
-    gmp_randclear(state);
+    printf("N = %llu = %llu * %llu\n", N, p, q);
 
     return 0;
 }
